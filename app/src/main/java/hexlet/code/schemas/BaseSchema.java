d9f5583ac20eb01ba892995b484b6d741779a5ea
andrg2280 <andrg2280.mail.ru>
2024-02-08 17:51:45 +0300

    refactoring

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,21 +4,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 
-import static java.lang.Boolean.TRUE;
 
-public abstract class BaseSchema<Object> {
+public abstract class BaseSchema {
     protected final List<Predicate<Object>> conditions;
-    private Boolean isRequired = false;
-
     protected BaseSchema() {
+
         this.conditions = new LinkedList<>();
     }
 
-    //Indicates if the specified attribute is required
-    protected final void setIsRequired() {
-        this.isRequired = TRUE;
-    }
     protected final void addCondition(Predicate<Object> condition) {
+
         conditions.add(condition);
     }
     public final boolean isValid(Object data) {
