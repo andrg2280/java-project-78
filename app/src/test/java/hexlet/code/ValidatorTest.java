b8ec5d3891ed235564037580c66c78a6d9023d7e
andrg2280 <andrg2280.mail.ru>
2024-02-12 18:58:07 +0300

    refactoring7

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -29,6 +29,15 @@ public class ValidatorTest {
         assertTrue(actual);
         actual = schema.isValid(null);
         assertTrue(actual);
+
+        assertTrue(schema.isValid(""));
+
+        schema.required();
+        assertTrue(schema.isValid("what does the fox say"));
+        assertTrue(schema.isValid("hexlet"));
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
+
         schema.required().contains("ab").minLength(5);
         actual = schema.required().contains("ab").minLength(5).isValid("abcdef");
         assertTrue(actual);
@@ -42,6 +51,7 @@ public class ValidatorTest {
         assertTrue(actual);
         actual = schema.minLength(10).isValid("ab");
         assertFalse(actual);
+
     }
 
     @Test
@@ -53,6 +63,8 @@ public class ValidatorTest {
         assertTrue(actual);
         actual = schema.isValid(null);
         assertTrue(actual);
+        actual = schema.required().isValid(null);
+        assertFalse(actual);
         actual = schema.required().positive().isValid(null);
         assertFalse(actual);
         actual = schema.required().isValid(null);
